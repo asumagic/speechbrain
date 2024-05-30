a19636696a7788c4fe188265d8e3212eaca0246d
asu <sdelang@sdelang.fr>
2024-05-30 16:48:47 +0200

    Add quirks.py with disabled CuDNN benchmarking

---
+++ speechbrain/utils/quirks.py
@@ -0,0 +1,50 @@
+"""Platform/GPU-specific quirks, i.e. workarounds and saner defaults due to
+platform-specific issues.
+
+Author:
+    * Sylvain de Langen 2024
+"""
+
+import torch
+import logging
+
+logger = logging.getLogger(__name__)
+
+def disable_cudnn_benchmarking():
+    """Disables CuDNN benchmarking. no-op on platforms where it is already off
+    by default.
+    
+    Benchmarking, when enabled, theoretically improves convolution performance
+    by automatically comparing different kernels for some operations.
+    
+    However, benchmarking has to be re-run for every unique input shape, which
+    makes it unsuitable for highly dynamic shapes.
+    Since SpeechBrain does tend to use very varied shapes without attempting to
+    pad the differences out, leaving benchmarking on can severely degrade
+    training performance.
+
+    As of PyTorch 2.3.0, the default is `False` for CUDA GPUs, but `True`
+    for HIP GPUs.
+    """
+
+    logger.info(
+        "... Setting `torch.backends.cudnn.benchmark = False`. "
+        "See `disable_cudnn_benchmarking` in `speechbrain/utils/quirks.py`."
+    )
+    torch.backends.cudnn.benchmark = False
+
+
+def apply_hip_quirks():
+    """Apply quirks specific to AMD HIP."""
+
+    logger.info("Detected AMD HIP. Applying HIP-specific quirks...")
+
+    disable_cudnn_benchmarking()
+
+
+def apply_quirks():
+    """Apply quirks depending on the platform."""
+
+    # AMD HIP?
+    if torch.cuda.is_available() and torch.version.hip:
+        apply_hip_quirks()
